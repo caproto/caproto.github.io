6cf208f6590190a6a3b68d27a63777bd3c6f5bc4
klauer <klauer@users.noreply.github.com>
2021-03-18 18:17:28 +0000

    deploy: caproto/caproto@22bbccba2f8087ff85279a75f80e34306e16a801

--- caproto/master/_static/documentation_options.js
+++ caproto/master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g04a5f9e',
+    VERSION: '0+untagged.1.g22bbccb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
